547aa5cbd5e34bfe00624964088b2852cad7f8d8
GoukiHino <g.hino@i5net.jp>
2023-07-20 16:07:06 +0900

    add: create access token function

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,23 @@
+import datetime
+
+from jose import jwt
+
 from passlib.context import CryptContext
 
+SECRET_KEY = "secret_key"
+ALGORITHM = "HS256"
+EXPIRE_MINUTES = 60 * 24 * 7
+
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+
+def create_access_token(sub: str) -> str:
+    to_encode = {
+        "sub": sub,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=EXPIRE_MINUTES)
+    }
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 def create_hashed_password(password: str) -> str:
