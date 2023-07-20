b2e16c7101c2f4c1af425cde11385bd2e3d73a07
GoukiHino <g.hino@i5net.jp>
2023-07-20 15:48:03 +0900

    add: user create endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
+from app.api.v1 import api
+from app.core.config import settings
+
 app = FastAPI()
-
-
-@app.get("/")
-def index():
-    return {"msg": "Hello, world!"}
+app.include_router(api.router, prefix=settings.API_PREFIX)
