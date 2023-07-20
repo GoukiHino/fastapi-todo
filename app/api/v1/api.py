9912ad7b23b1f0225bdaf10335578d1d135b4fcb
GoukiHino <g.hino@i5net.jp>
2023-07-20 16:27:56 +0900

    add: login endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import user
+from app.api.v1.endpoints import login, user
 
 router = APIRouter()
+router.include_router(login.router, tags=["login"])
 router.include_router(user.router, prefix="/users", tags=["users"])
