7778e3c56dea319c12c69745a971d371a788f52a
GoukiHino <g.hino@i5net.jp>
2023-07-21 11:24:04 +0900

    add: user endpoints

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,4 +1,16 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+from sqlalchemy.orm import Session
+
+from jose import jwt
+
+from app import cruds, models, schemas
+from app.core.config import settings
+from app.core.security import SECRET_KEY, ALGORITHM
 from app.db.session import SessionLocal
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_PREFIX}/login")
 
 
 def get_db():
@@ -7,3 +19,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> models.User:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        token_data = schemas.TokenPayload(**payload)
+    except jwt.JWTError:
+        raise HTTPException(status_code=403, detail="Could not validate credentials")
+    user = cruds.user.get(token_data.sub, db)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -9,5 +9,32 @@ router = APIRouter()
 
 
 @router.post("/", response_model=schemas.UserResponse)
-def create(obj_in: schemas.UserCreate, db: Session = Depends(deps.get_db)):
+def create(
+        obj_in: schemas.UserCreate,
+        db: Session = Depends(deps.get_db)
+):
     return cruds.user.create(obj_in, db)
+
+
+@router.get("/me", response_model=schemas.UserResponse)
+def get(
+        current_user: models.User = Depends(deps.get_current_user)
+):
+    return current_user
+
+
+@router.put("/me", response_model=schemas.UserResponse)
+def update(
+        obj_in: schemas.UserUpdate,
+        current_user: models.User = Depends(deps.get_current_user),
+        db: Session = Depends(deps.get_db)
+):
+    return cruds.user.update(current_user.username, obj_in, db)
+
+
+@router.delete("/me", response_model=schemas.UserResponse)
+def delete(
+        current_user: models.User = Depends(deps.get_current_user),
+        db: Session = Depends(deps.get_db)
+):
+    return cruds.user.delete(current_user.username, db)
